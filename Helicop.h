fdc1c814bbe80b9a3ff8d38c2270f28d45f490ee
Efrain Rios Durazo <efrainrd05@gmail.com>
2026-02-18 08:37:51 -0800

    Declaring the Helicopter class with Private members and public mechanics

--- Helicop.h
+++ Helicop.h
@@ -1,4 +1,19 @@
 #ifndef HELICOP_H
 #define HELICOP_H
 
+#include <string>
 
+class Helicop
+{
+private:
+	int altitude;
+	int distance;
+
+
+public:
+	helicop(); // constructor
+
+	void ascend();
+	void descend();
+	void forward();
+	void land();
